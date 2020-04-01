7a56cc0c5e3847fe98c9f3dfc12f5a62d4d7f618
AmenoCoyane <bobbidigi11@gmail.com>
2020-04-01 14:38:50 -0400

    MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-
 import SavedList from './Movies/SavedList';
 import { Route } from 'react-router-dom';
 import { BrowserRouter as Router } from "react-router-dom";
 import { Link } from 'react-router-dom';
 import  MovieList  from './Movies/MovieList'
 import  Movie  from './Movies/Movie'
+import { useParams } from "react-router-dom";
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -18,7 +18,8 @@ const App = () => {
       <div>
         <SavedList list={savedList} />
           <Route exact path="/" component={MovieList}/>
-          <Route path="/movies/:id" component={Movie}/>
+          {/* <Route exact path="/movies/:movieId" component={Movie}/> */}
+          <Route path="/movies/:id" render={props => (<Movie {...props}/>)}/>
       </div>
   );
 };
